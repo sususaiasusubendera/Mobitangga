d04ab2cb9240de64f646a1c1d6fb3a8bc7a6d0ce
DnA2702 <88304350+DnA2702@users.noreply.github.com>
2021-11-20 15:06:30 +0700

    Update roll.c

--- roll.c
+++ roll.c
@@ -13,7 +13,7 @@ buff = 5 -> Mesin Penukar Posisi
 
 int rollDadu(int buff, int N){
 	int MaxRoll, MinRoll, Roll, i, result;
-	MaxRoll = N-1;
+	MaxRoll = floor(N/2);
 	MinRoll = 1;
 	if (buff == 3){
 		MinRoll = floor(MaxRoll/2) ;
