c781b043ba5e13f79e9adcdc7e6276104d799f9c
Ray Clement Juwanda <rayclementj@gmail.com>
2021-11-22 21:16:20 +0700

    Pembaruan ADT Stack
    Menjadi memiliki playerstate yg mengandung state dari tiap pemain

--- ADT/stack.c
+++ ADT/stack.c
@@ -3,45 +3,31 @@
 
 #include "../boolean.h"
 #include "stack.h"
+#include "player.h"
 #include <stdlib.h>
 
 /* *** Konstruktor/Kreator *** */
-void NewGame (Stack *S){
-/* I.S. sembarang; */
-/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
-/* jadi indeksnya antara 1.. MaxEl+1 karena 0 tidak dipakai */
-/* Ciri stack kosong : round bernilai Nil */
+void CreateStack (Stack *S){
     round(*S) = Nil;
 }
 
-
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsNewGame (Stack S){
-/* Mengirim true jika Stack kosong: lihat definisi di atas */
+boolean IsStackEmpty (Stack S){
     return round(S) == Nil;
 }
 
-boolean IsEndGame (Stack S){
-/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+boolean IsStackFull (Stack S){
     return round(S) == MaxEl;
 }
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void AddRound (Stack * S, infotype X, infotype Y){
-/* Menambahkan X sebagai elemen Stack S. */
-/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
-/* F.S. X dan Y menjadi p1position dan p2position yang baru, round bertambah 1 */
+void Push (Stack * S, TabPlayer P){
     round(*S)=round(*S)+1;
-    p1position(*S)=X;
-    p2position(*S)=X;
+    playerstate(*S)= P;
 }
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void UndoRound (Stack * S, infotype* X, infotype* Y){
-/* Menghapus X dari Stack S. */
-/* I.S. S  tidak mungkin kosong */
-/* F.S. X dan Y adalah nilai elemen p1position dan p2position yang lama, round berkurang 1 */
-    *X = p1position(*S);
-    *Y = p2position(*S);
+void Pop (Stack * S, TabPlayer* P){
+    *P = playerstate(*S);
     round(*S)=round(*S)-1;
 }
--- ADT/stack.h
+++ ADT/stack.h
@@ -6,59 +6,59 @@
 #define stackt_H
 
 #include "../boolean.h"
-
+#include "player.h"
 
 #define Nil 0
 #define MaxEl 20
+
 /* Nil adalah stack dengan elemen kosong . */
 /* Karena indeks dalam bhs C dimulai 0 maka tabel dg indeks 0 tidak dipakai */
-
 typedef int infotype;
 typedef int address;   /* indeks tabel */
-
 /* Contoh deklarasi variabel bertype stack dengan ciri round : */
 /* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
+
 typedef struct {
-    infotype p1position[MaxEl+1]; /* tabel penyimpan posisi pemain pertama */
-    infotype p2position[MaxEl+1]; /* tabel penyimpan posisi pemain kedua */
+    TabPlayer playerstate; /* tabel penyimpan state dari pemain */
     address round;  /* alamat TOP: elemen puncak */
 } Stack;
+
 /* Definisi stack S kosong : S.round = Nil */
 /* Elemen yang dipakai menyimpan nilai Stack p1position[1]..p1position[MaxEl] */
-/* Elemen yang dipakai menyimpan nilai Stack p2position[1]..p2position[MaxEl] */
 /* Jika S adalah Stack maka akses elemen : */
    /* S.player[(S.round)] untuk mengakses elemen round */
    /* S.round adalah alamat elemen round */
 
 /* Definisi akses dengan Selektor : Set dan Get */
 #define round(S) (S).round
-#define p1position(S) (S).p1position[(S).round]
-#define p2position(S) (S).p2position[(S).round]
+#define playerstate(S) (S).playerstate[(S).round]
 
 /* ************ Prototype ************ */
 /* *** Konstruktor/Kreator *** */
-void NewGame (Stack *S);
+void CreateStack (Stack *S);
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 1.. MaxEl+1 karena 0 tidak dipakai */
 /* Ciri stack kosong : round bernilai Nil */
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsNewGame (Stack S);
+boolean IsStackEmpty (Stack S);
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
-boolean IsEndGame (Stack S);
+boolean IsStackFull (Stack S);
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void AddRound (Stack * S, infotype X, infotype Y);
+void Push (Stack * S, TabPlayer playerstate);
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
-/* F.S. X dan Y menjadi p1position dan p2position yang baru, round bertambah 1 */
+/* F.S. P akan menjadi playerstate yang baru, 
+skillA dan skillB menjadi p1skill dan p2skill yang baru, round bertambah 1 */
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void UndoRound (Stack * S, infotype* X, infotype* Y);
+void Pop (Stack * S, TabPlayer* P);
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
-/* F.S. X dan Y adalah nilai elemen p1position dan p2position yang lama, round berkurang 1 */
+/* F.S. P adalah nilai dari playerstate yang lama,
+skillA dan skillB menjadi p1skill dan p2skill adalah skill terakhir yang didapatkan sebelumnya, round berkurang 1 */
 
 #endif
