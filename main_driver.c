883b5da63233702ecbac50b3b8cbb3e94ca7fbe1
DnA2702 <88304350+DnA2702@users.noreply.github.com>
2021-11-22 11:04:03 +0700

    Add files via upload

---
+++ main_driver.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "game_flow.h"
+
+int main(){
+	boolean endGame;
+	intro();
+	char command[MaxCommand];
+	puts("1. NEW GAME");
+	puts("2. EXIT");
+	printf("Masukan command : ");
+	gets(command);
+	endGame = false;
+	while (strcmp(command, "NEW GAME") != 0 && strcmp(command, "EXIT") != 0){
+		printf("Masukan command : ");
+		gets(command);
+	}
+	if (strcmp(command, "NEW GAME") == 0){
+		while (!endGame){
+			endGame = game(endGame);
+		}
+		puts("Terima kasih telah memainkan game ini :)\n");
+	}
+	else{
+		puts("Terima kasih telah memainkan game ini :)\n");
+	}
+	return 0;
+}
