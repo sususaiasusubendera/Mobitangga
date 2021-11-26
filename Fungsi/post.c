68183d65ce3848bbe34e0cb315ff10c86a4866c0
ino <82642736+sususaiasusubendera@users.noreply.github.com>
2021-11-27 00:03:12 +0700

    Create post.c

---
+++ Fungsi/post.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "post.h"
+
+void outro() {
+    printf("                                                                                                                                                                          \n");
+    printf(" .o88b.           db    db           d888888b d8b   db      d888888b db   db d88888b      d8b   db d88888b db    db d888888b       d888b   .d8b.  .88b  d88. d88888b      \n");
+    printf("d8P  Y8           88    88             `88'   888o  88      `~~88~~' 88   88 88'          888o  88 88'     `8b  d8' `~~88~~'      88' Y8b d8' `8b 88'YbdP`88 88'          \n");
+    printf("8P                88    88              88    88V8o 88         88    88ooo88 88ooooo      88V8o 88 88ooooo  `8bd8'     88         88      88ooo88 88  88  88 88ooooo      \n");
+    printf("8b                88    88              88    88 V8o88         88    88~~~88 88~~~~~      88 V8o88 88~~~~~  .dPYb.     88         88  ooo 88~~~88 88  88  88 88~~~~~      \n");
+    printf("Y8b  d8           88b  d88             .88.   88  V888         88    88   88 88.          88  V888 88.     .8P  Y8.    88         88. ~8~ 88   88 88  88  88 88.          \n");
+    printf(" `Y88P'           ~Y8888P'           Y888888P VP   V8P         YP    YP   YP Y88888P      VP   V8P Y88888P YP    YP    YP          Y888P  YP   YP YP  YP  YP Y88888P      \n");
+    printf("\n");
+    printf("                                                                                                                                                    SAYONARAAAAAAA ~\n");
+    printf("                                                                                                                                                                   ;)");
+    printf("\n");
+}
